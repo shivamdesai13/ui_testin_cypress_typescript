ca29b7b0d56d87709bc99ebf4303f6580af79753
shivamdesai13 <shivamdesai13@gmail.com>
2022-05-20 14:22:06 +0530

    initial commit

---
+++ cypress/integration/Register.Test.ts
@@ -0,0 +1,41 @@
+describe("test for registration", () => {
+    let elementJson;
+    let userJson;
+    beforeEach(() => {
+      cy.visit("/Register.html"); //to visit website
+  
+      cy.fixture("elementId").then(function (data) {
+        elementJson = data;
+      });
+      cy.fixture("userData").then(function (data) {
+        userJson = data;
+      });
+    });
+    it("register test", () => {
+      cy.enterText(elementJson.enterName, userJson.name); //enter name
+      cy.enterText(elementJson.enterLastName, userJson.lastName); //enter last name
+      cy.enterText(elementJson.enterAddress, userJson.address); //enter address
+      cy.enterText(elementJson.enterEmail, userJson.email); //email id
+      cy.enterText(elementJson.mobNo, userJson.number); //contact number
+      cy.clickBtn(elementJson.genderF); //select gender
+      cy.clickBtn(elementJson.hobby1); //enter hobbies
+      cy.clickBtn(elementJson.language); //click on language field
+      cy.contains(userJson.languages).click(); //enter language
+      cy.clickBtn(elementJson.hobby2); //enter hobbies 2
+      cy.selectBtn(elementJson.skills, userJson.skillss); //enter skills
+      cy.clickBtn(elementJson.selectCountry); //click on select country
+      
+      cy.enterText(elementJson.enterCountry, userJson.country); //fill field for select country and enter
+      cy.selectBtn(elementJson.birthYear, userJson.bYear); //birth year
+      cy.selectBtn(elementJson.birthMonth, userJson.bMonth); // birth month
+      cy.selectBtn(elementJson.birthDate, userJson.bDate); // birth date
+      cy.enterText(elementJson.registerPassword, userJson.passwordreg); //enter password
+      cy.enterText(
+        elementJson.registerPasswordconfirm,
+        userJson.passwordregconfirm
+      ); //enter confirm password
+      cy.clickBtn(elementJson.selectFile).selectFile(userJson.file); // select file
+      cy.clickBtn(elementJson.submitBtn); //submit form
+    });
+  });
+  
---
+++ cypress/support/commands.ts
@@ -0,0 +1,39 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+Cypress.Commands.add('clickBtn', (btnNm) => {
+    cy.get(btnNm).click({force:true})
+})
+
+Cypress.Commands.add('enterText', (eId,text) => {
+    cy.get(eId).type(text)
+})
+
+Cypress.Commands.add('selectBtn', (select,text) => {
+    cy.get(select).select(text)
+})
+
+
+
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
